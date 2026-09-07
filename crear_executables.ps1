630a3530f9ac95ca41dc8f3448a1922e2e2ea3fb
Zephyr <adrielprog2a@gmail.com>
2026-09-07 20:24:11 +0200

    Delete crear_executables.ps1

--- crear_executables.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-Set-Location $PSScriptRoot
-
-pyinstaller --noconfirm --clean --windowed --onefile --name AppLocker --paths Cifrado --distpath Cifrado --workpath Cifrado\build --specpath Cifrado Cifrado\main.py
-
-Write-Host "Ejecutable creado en Cifrado\AppLocker.exe"
